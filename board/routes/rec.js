d554d15e4c8ca542bba3fdc2643cd19b242fddc6
Terryu\Terry <anhj0131@naver.com>
2018-02-09 12:08:33 +0900

    THIS

--- board/routes/rec.js
+++ board/routes/rec.js
@@ -4,29 +4,72 @@ var User = require("../models/User");
 var passport = require("../config/passport");
 var mongoose = require("mongoose");
 var fs = require('fs');
+var recombee = require('recombee-api-client');
+var rqs = recombee.requests;
+var client = new recombee.ApiClient('csee', 'mkQHj8YcBaHnGyTimeUrsIPTtXNnL39s68ArLgkojjcGllJXOPacbQryBbVHI14R');
+
 
 var Schema = mongoose.Schema;
 
-router.get("/", isLoggedIn, function(req, res){
-  res.render("rec/reco", { user : req.user });
+
+router.get("/", isLoggedIn, function(req, res) {
+  res.render("rec/reco", {
+    user: req.user
+  });
 });
 
-router.post("/sending", isLoggedIn, function(req, res){
+router.post("/sending", isLoggedIn, function(req, res) {
+
+  var school = req.body.q21_input21;
+  var career = req.body.q23_input23;
+  var english = req.body.q22_input22;
+  var score = req.body.q20_input20;
+  var job = req.body.detail;
+  var userID = req.user.username;
+
+
+  console.log(school, career, english, score, userID);
+
+  client.send(new rqs.AddUser(userID));
+
+  client.send(new rqs.Batch([new rqs.ResetDatabase(),
+      new rqs.AddUserProperty('school', 'string'),
+      new rqs.AddUserProperty('career', 'string'),
+      new rqs.AddUserProperty('english', 'string'),
+      new rqs.AddUserProperty('score', 'int'),
+      new rqs.AddUserProperty('job', 'string')
+    ]))
+.then((responses) => {
+
+      client.send(new rqs.SetUserValues(
+        userID,
+        {
+            'school':  req.body.q21_input21,
+            'career': req.body.q23_input23,
+            'english': req.body.q22_input22,
+            'score': score,
+            'job': req.body.detail
+          },
+          //optional parameters:
+          {
+            'cascadeCreate': true // Use cascadeCreate for creating user
+            // with given userId, if it doesn't exist
+          }
+        ));
+          return client.send(new rqs.Batch(requests));
+})
+
   res.render("rec/sending");
-});
-
-router.get('/load/:name',isLoggedIn, function(req,res){
-	res.render('rec/'+req.params.name);
 });
 
 module.exports = router;
 
 function isLoggedIn(req, res, next) {
 
-    // if user is authenticated in the session, carry on
-    if (req.isAuthenticated())
-        return next();
+  // if user is authenticated in the session, carry on
+  if (req.isAuthenticated())
+    return next();
 
-    // if they aren't redirect them to the home page
-    res.render("home/welcome");
+  // if they aren't redirect them to the home page
+  res.render("home/welcome");
 }
