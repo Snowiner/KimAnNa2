c6f2d6e3b58d9a78b261397e402eca24e206a57d
NTH413-PC\NTH413 <test@test.com>
2018-02-22 11:25:48 +0900

    첫번째 직무추천

--- board/routes/rec.js
+++ board/routes/rec.js
@@ -22,25 +22,44 @@ router.get("/", isLoggedIn, function(req, res) {
 });
 
 router.post("/sending", isLoggedIn, function(req, res) {
+  var school = 0;
+  if (req.body.q21_input21 == "4년제 대학 졸업") {
+    school = 4;
+  } else if (req.body.q21_input21 == "전문 대학 졸업") {
+    school = 2;
+  } else {
+    school = 1;
+  }
 
-  var school = req.body.q21_input21;
-  var career = req.body.q23_input23;
-  var english = req.body.q22_input22.toLowerCase();
+  var career = 0;
+  if (req.body.q23_input23 == "신입") {
+    career = 1;
+  } else if (req.body.q23_input23 == "3년 미만") {
+    career = 3;
+  } else if (req.body.q23_input23 == "5년 미만") {
+    career = 5;
+  } else {
+    career = 7;
+  }
+
+  var english = req.body.q22_input22;
   var score = req.body.q20_input20;
   var job = req.body.detail;
   var userID = req.user.username;
 
 
-  console.log(school, career, english, score, userID, job);
+  console.log(school, career);
 
   //client.send(new rqs.AddUser(userID));
+
 
   client.send(new rqs.Batch([ //new rqs.ResetDatabase(),
       new rqs.AddUserProperty('school', 'string'),
       new rqs.AddUserProperty('career', 'string'),
       new rqs.AddUserProperty('english', 'string'),
       new rqs.AddUserProperty('score', 'int'),
-      new rqs.AddUserProperty('job', 'string')
+      new rqs.AddUserProperty('job', 'string'),
+      new rqs.AddUserProperty('field', 'string')
     ]))
     .then((responses) => {
 
@@ -48,9 +67,10 @@ router.post("/sending", isLoggedIn, function(req, res) {
         userID, {
           'school': req.body.q21_input21,
           'career': req.body.q23_input23,
-          'english': req.body.q22_input22.toLowerCase(),
+          'english': req.body.q22_input22,
           'score': score,
-          'job': req.body.detail
+          'job': req.body.detail,
+          'field': req.body.field
         },
         //optional parameters:
         {
@@ -61,25 +81,29 @@ router.post("/sending", isLoggedIn, function(req, res) {
       //return client.send(new rqs.Batch(requests));
     })
 
-  client.send(new rqs.Batch([
-      new rqs.AddItemProperty('school', 'string'),
-      new rqs.AddItemProperty('career', 'string'),
-      new rqs.AddItemProperty('toeic', 'int'),
-      new rqs.AddItemProperty('toefl', 'int'),
-      new rqs.AddItemProperty('teps', 'int'),
-      new rqs.AddItemProperty('job', 'string')
+  client.send(new rqs.Batch([ //new rqs.ResetDatabase(),
+      new rqs.AddItemProperty('school', 'int'),
+      new rqs.AddItemProperty('career', 'int'),
+      new rqs.AddItemProperty('TOEIC', 'int'),
+      new rqs.AddItemProperty('TOEFL', 'int'),
+      new rqs.AddItemProperty('TEPS', 'int'),
+      new rqs.AddItemProperty('job', 'string'),
+      new rqs.AddItemProperty('field', 'string'),
+      new rqs.AddItemProperty('img', 'string')
     ]))
 
     .then((responses) => {
 
       client.send(new rqs.SetItemValues(
         "Johnshopkins", {
-          'school': '4년제 대학 졸업',
-          'career': '5년 이상',
-          'toeic': 900,
-          'toefl': 100,
-          'teps': 880,
-          'job': '의사'
+          'school': '4',
+          'career': '5',
+          'TOEIC': 900,
+          'TOEFL': 100,
+          'TEPS': 880,
+          'job': '의사',
+          'field': 'medical',
+          'img': 'http://cfile30.uf.tistory.com/image/232BF44954A664CA34B995'
         },
         //optional parameters:
         {
@@ -89,12 +113,14 @@ router.post("/sending", isLoggedIn, function(req, res) {
       ));
       client.send(new rqs.SetItemValues(
         "Samsung-sds", {
-          'school': '4년제 대학 졸업',
-          'career': '5년 이상',
-          'toeic': 700,
-          'toefl': 80,
-          'teps': 700,
-          'job': '보안 개발자'
+          'school': '4',
+          'career': '5',
+          'TOEIC': 700,
+          'TOEFL': 80,
+          'TEPS': 700,
+          'job': '보안 개발자',
+          'field': 'developer',
+          'img': 'http://vietnam-manufacturing.com/wp-content/uploads/2016/08/samsung-logo-540x334-3.jpg'
         },
         //optional parameters:
         {
@@ -104,12 +130,14 @@ router.post("/sending", isLoggedIn, function(req, res) {
       ));
       client.send(new rqs.SetItemValues(
         "LG-CNS", {
-          'school': '4년제 대학 졸업',
-          'career': '3년 이상',
-          'toeic': 750,
-          'toefl': 70,
-          'teps': 800,
-          'job': '파이썬 개발자'
+          'school': '4',
+          'career': '3',
+          'TOEIC': 750,
+          'TOEFL': 70,
+          'TEPS': 800,
+          'job': '파이썬 개발자',
+          'field': 'developer',
+          'img': 'http://cfile21.uf.tistory.com/image/266DBA4F536059710361FE'
         },
         //optional parameters:
         {
@@ -119,12 +147,14 @@ router.post("/sending", isLoggedIn, function(req, res) {
       ));
       client.send(new rqs.SetItemValues(
         "SK", {
-          'school': '4년제 대학 졸업',
-          'career': '신입',
-          'toeic': 750,
-          'toefl': 80,
-          'teps': 700,
-          'job': 'PHP 개발자'
+          'school': '4',
+          'career': '1',
+          'TOEIC': 750,
+          'TOEFL': 80,
+          'TEPS': 700,
+          'job': 'PHP 개발자',
+          'field': 'developer',
+          'img': 'http://biz.newdaily.co.kr/data/photos/20160622/art_1464827886.jpg'
         },
         //optional parameters:
         {
@@ -134,12 +164,14 @@ router.post("/sending", isLoggedIn, function(req, res) {
       ));
       client.send(new rqs.SetItemValues(
         "Google_korea", {
-          'school': '4년제 대학 졸업',
-          'career': '5년 이상',
-          'toeic': 900,
-          'toefl': 120,
-          'teps': 800,
-          'job': '네트워크 관리자'
+          'school': '4',
+          'career': '5',
+          'TOEIC': 900,
+          'TOEFL': 120,
+          'TEPS': 800,
+          'job': '네트워크 관리자',
+          'field': 'developer',
+          'img': 'https://www.allkpop.com/upload/2016/07/af_org/misc_1467825206_af_org.jpg'
         },
         //optional parameters:
         {
@@ -149,37 +181,52 @@ router.post("/sending", isLoggedIn, function(req, res) {
       ));
       client.send(new rqs.SetItemValues(
         "Nexon", {
-          'school': '4년제 대학 졸업',
-          'career': '3년 이상',
-          'toeic': 700,
-          'toefl': 80,
-          'teps': 700,
-          'job': '유니티 개발자'
+          'school': '4',
+          'career': '3',
+          'TOEIC': 700,
+          'TOEFL': 80,
+          'TEPS': 700,
+          'job': '유니티 개발자',
+          'field': 'developer',
+          'img': 'http://s.nx.com/S2/p3/main/nexon.png'
         },
         //optional parameters:
         {
           'cascadeCreate': true // Use cascadeCreate for creating user
           // with given userId, if it doesn't exist
         }
+
       ));
-      //return client.send(new rqs.Batch(requests));
     })
 
-//console.log("갺",english,score);
-  client.send(new rqs.ItemBasedRecommendation('LG-CNS', 1, {
-    'targetUserId':userID,
-    //'filter': "'job' = job"
-  }))
-  .then((recommended) => {
-    console.log(recommended);
-  });
+  // client.send(new rqs.AddPurchase(userID,itemID, {
+  //   'cascadeCreate': true
+  // }));
+
+  var english_filter = "'" + req.body.q22_input22 + "'" + "<=" + score;
+  var school_filter = "'" + 'school' + "'" + "<=" + school;
+  var career_filter = "'" + 'career' + "'" + "<=" + career;
+  //"    'TOEFL'  >=90      "
+
+  client.send(new rqs.UserBasedRecommendation(userID, 1, {
+      'filter': english_filter,
+      school_filter,
+      career_filter,
+      'returnProperties': true
+    }))
+    .then((recommended) => {
+
+      console.log(recommended);
+      res.render("rec/sending", {
+        recommended : recommended
+      });
+
+
+    });
 
 
 
-
-
-
-  res.render("rec/sending");
+  // res.render("rec/sending");
 });
 
 
